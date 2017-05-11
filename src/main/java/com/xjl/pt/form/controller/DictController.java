878aa547ed159a292d773f4711b4ac0d7e7c5846
123 <123@192.168.1.103>
2017-05-11 12:11:40 +0800

    增加字典的增删改查

--- src/main/java/com/xjl/pt/form/controller/DictController.java
+++ src/main/java/com/xjl/pt/form/controller/DictController.java
@@ -9,13 +9,16 @@ import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.pagehelper.Page;
 import com.xjl.pt.core.domain.Dict;
+import com.xjl.pt.core.domain.User;
 import com.xjl.pt.core.service.DictService;
+import com.xjl.pt.core.service.UserService;
 import com.xjl.pt.form.domain.Form;
 /**
  * 字典控制器类
@@ -27,6 +30,8 @@ import com.xjl.pt.form.domain.Form;
 public class DictController {
 	@Autowired
 	private DictService dictService;
+	@Autowired
+	private UserService userService;
 	@ResponseBody
 	@RequestMapping(value="/all",method=RequestMethod.GET,consumes = "application/json")
 	public BootstrapGridTable all(HttpServletRequest request){
@@ -35,5 +40,28 @@ public class DictController {
 		String search = StringUtils.trimToNull(request.getParameter("search"));
 		List<Dict> list = this.dictService.queryByName(search, page, rows);
 		return BootstrapGridTable.getInstance(list);
+	}
+	@ResponseBody
+	@RequestMapping(value="/add",method=RequestMethod.POST,consumes = "application/json")
+	public void add(@RequestBody Dict dict){
+		User user = this.userService.queryById("9fcfdb3e-3bdb-4234-a0c4-f91d023c308e");
+		this.dictService.add(dict, user);
+		
+	}
+	@ResponseBody
+	@RequestMapping(value="/modify",method=RequestMethod.POST,consumes = "application/json")
+	public void modify(@RequestBody Dict dict){
+		User user = this.userService.queryById("9fcfdb3e-3bdb-4234-a0c4-f91d023c308e");
+		this.dictService.modify(dict, user);
+	}
+	@ResponseBody
+	@RequestMapping(value="/delete",method=RequestMethod.POST,consumes = "application/json")
+	public void delete(@RequestBody List<Dict> list){
+		User user = this.userService.queryById("9fcfdb3e-3bdb-4234-a0c4-f91d023c308e");
+		for (Dict dict : list) {
+			System.out.println(dict.getDictId() + ":" + dict.getDictName());
+			this.dictService.delete(dict, user);
+		}
+		
 	}
 }
