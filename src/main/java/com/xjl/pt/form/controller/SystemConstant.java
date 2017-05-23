dc372fe432812acdcdd229d7a2111385583a596b
guanzheyuan <834155417@qq.com>
2017-05-23 15:42:14 +0800

    定义系统常量文件

---
+++ src/main/java/com/xjl/pt/form/controller/SystemConstant.java
@@ -0,0 +1,48 @@
+package com.xjl.pt.form.controller;
+/**
+ * 定义系统常量
+ * @author guan.zheyuan
+ */
+public class SystemConstant {
+
+	/**
+	 * ftp账号
+	 */
+	public  static final String FTP_NAME="ftpxjl";
+	/**
+	 * ftp密码 
+	 */
+	public static final String FTP_PASSWORD="A6CCxjl";
+	/**
+	 * ftp服务器地址
+	 */
+	public static final String FTP_IP="123.57.4.104";
+	/**
+	 * ftp目录地址
+	 */
+	public static final String FTP_PATH="/home/ftpxjl";
+	/**
+	 * 备份目录
+	 */
+	public static final String BACKUP_FOLDER="/upload/";
+	/**
+	 * 证照：正面
+	 */
+	public static final String SIGN_FRONT="front";
+	public static final String SIGN_FRONT_VALUE="_FRONT";
+	/**
+	 * 证照：反面
+	 */
+	public static final String SIGN_BACK="back";
+	public static final String SIGN_BACKA_VALUE="_BACK";
+	/**
+	 * 证照：手持
+	 */
+	public static final String SIGN_HAND="hand";
+	public static final String SIGN_HAND_VALUE="_HAND";
+	
+	/**
+	 * Session中的用户key（不可修改）
+	 */
+	public static final String SESSION_USER = "SESSION_USER";
+}
