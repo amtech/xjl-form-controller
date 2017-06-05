9d792b85c71d8b20d4dbac1aa611122f1b2404bb
guanzheyuan <834155417@qq.com>
2017-06-05 14:12:58 +0800

    去除不需要的引入

--- src/main/java/com/xjl/pt/form/interceptor/LogInterceptor.java
+++ src/main/java/com/xjl/pt/form/interceptor/LogInterceptor.java
@@ -1,26 +1,14 @@
 package com.xjl.pt.form.interceptor;
-
 import java.io.UnsupportedEncodingException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
-
-import com.sun.org.apache.bcel.internal.generic.NEW;
 import com.xjl.pt.core.domain.User;
 import com.xjl.pt.core.domain.UserLog;
 import com.xjl.pt.core.domain.XJLDomain;
@@ -62,41 +50,42 @@ public class LogInterceptor implements HandlerInterceptor{
   		 String contextPath = request.getContextPath();
   		 final String url = requestUri.substring(contextPath.length());
 		//添加用户信息
-		//final User userDefault = userService.queryById("9fcfdb3e-3bdb-4234-a0c4-f91d023c308e");
+		final User userDefault = userService.queryById("9fcfdb3e-3bdb-4234-a0c4-f91d023c308e");
 		//异步调用
-//		new Thread(){
-//            public void run() {
-//            		 String city ="";
-//                	 String  userId ="";
-//                	 String addIp ="";
-//           		 String userName = "";
-//           		 log.debug("url:" + url);
-//           		 log.debug("addIp:" + addIp);
-//           		 //通过ip地址定位城市
-//				try {
-//					addIp = locationController.getWebIP(SystemConstant.LOG_GETIP);
-//					city = new locationController().getAddresses(addIp,"utf-8");
-//				} catch (UnsupportedEncodingException e) {
-//					e.printStackTrace();
-//				}
-//           		 log.debug("city:" + city);
-//           		 //得到用户信息，如果未登录访问则暂时不存用户信息
-//           		 if (null != user) {
-//           			 userId = String.valueOf(user.getUserId());
-//           			 userName = String.valueOf(user.getUserName());
-//           		}
-//           		//组装参数
-//				UserLog userLog = new UserLog();
-//				userLog.setIp(addIp);
-//				userLog.setCity(city);
-//				userLog.setUrl(url);
-//				userLog.setUserId(userId);
-//				userLog.setUserName(userName);
-//				userLog.setState(XJLDomain.StateType.A.name());
-//				userLogService.add(userLog,null);
-//            }
-//        }.start();
+		new Thread(){
+            public void run() {
+            		 String city ="";
+                	 String  userId ="";
+                	 String addIp ="";
+           		 String userName = "";
+           		 log.debug("url:" + url);
+           		 log.debug("addIp:" + addIp);
+           		 //通过ip地址定位城市
+				try {
+					addIp = locationController.getWebIP(SystemConstant.LOG_GETIP);
+					city = new locationController().getAddresses(addIp,"utf-8");
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
+           		 log.debug("city:" + city);
+           		 //得到用户信息，如果未登录访问则暂时不存用户信息
+           		 if (null != user) {
+           			 userId = String.valueOf(user.getUserId());
+           			 userName = String.valueOf(user.getUserName());
+           		}
+           		//组装参数
+				UserLog userLog = new UserLog();
+				userLog.setIp(addIp);
+				userLog.setCity(city);
+				userLog.setUrl(url);
+				userLog.setUserId(userId);
+				userLog.setUserName(userName);
+				userLog.setState(XJLDomain.StateType.A.name());
+				userLogService.add(userLog,userDefault);
+            }
+        }.start();
 	}
 	
+ 
 	
 }
