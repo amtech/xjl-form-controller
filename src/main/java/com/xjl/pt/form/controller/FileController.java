38825c2b71d442f6fc6f80fab6c03f059c86e635
guanzheyuan <834155417@qq.com>
2017-06-16 13:08:12 +0800

    修改文件上传调用

--- src/main/java/com/xjl/pt/form/controller/FileController.java
+++ src/main/java/com/xjl/pt/form/controller/FileController.java
@@ -4,10 +4,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.SocketException;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,9 +28,9 @@ import org.apache.commons.net.ftp.FTPClient;
  * 文件控制器
  * @author guan.zheyuan
  */
-@SuppressWarnings("restriction")
 @Controller
 @RequestMapping("/file")
+@SuppressWarnings("rawtypes")
 public class FileController {
 	
 	private static final Log log = LogFactory.getLog(FileController.class);
@@ -38,12 +38,64 @@ public class FileController {
 	/**
 	 * 富文本文件上传
 	 */
-	@SuppressWarnings({ "unchecked", "unused", "rawtypes"})
 	@RequestMapping(value="/uploadEditBox")
 	public void fileUploadEditBox(HttpServletRequest request,HttpServletResponse response){
 		uploadFTP(request, response,SystemConstant.FTP_PATH_EDITBOX);
 	}
-	private void uploadFTP(HttpServletRequest request, HttpServletResponse response,String type) {
+	/**
+	 * 执行证照上传
+	 */
+	@RequestMapping(value="/uploadFile")
+	public void fileUpload(HttpServletRequest request,HttpServletResponse response){
+		this.uploadFTP(request, response, SystemConstant.FTP_PATH_LICENCE);
+	}
+	 
+	/**
+	 * 控制器调用 返回上传信息
+	 * @param request
+	 * @param response
+	 * @param type
+	 * @return
+	 */
+	public Map<String,Object> uploadFTPForController(HttpServletRequest request,HttpServletResponse response,String type){
+		 Map<String, Object> tempMap = null;
+		List<FileItem> fileList = getFileList(request);  
+		log.debug("文件数量:" + fileList.size());
+		 //迭代器,搜索前端发送过来的文件
+		for (FileItem item : fileList) {
+    	 	//判断该表单项是否是普通类型
+         if (!item.isFormField()) {
+        	 String name = item.getName();
+        	 log.debug("name:" + name);
+             if (StringUtils.isBlank(name)) {
+                 continue;
+             }
+             File saveFile = this.getTempFile(request, name,SystemConstant.BACKUP_FOLDER);
+             log.debug("saveFile:" + saveFile.getName());
+             try {
+            	 	log.debug("开始写入临时文件");
+	            	item.write(saveFile);
+	            	log.debug("开始上传ftp");
+	            String ftpURL = uploadFtp(saveFile,type);
+	            log.debug("ftp地址：" + ftpURL);
+	            tempMap = new HashMap<>();
+	            tempMap.put("fileName", saveFile.getName());
+	            tempMap.put("ftpURL", ftpURL);
+             } catch (Exception e) {
+                 throw new RuntimeException(e);
+             }
+         }
+	}
+		return tempMap;
+	}
+	/**
+	 * 上传ftpf 输出页面
+	 * @param request
+	 * @param response
+	 * @param type
+	 */
+	@SuppressWarnings("unchecked")
+	public void uploadFTP(HttpServletRequest request, HttpServletResponse response,String type) {
 		List<FileItem> fileList = getFileList(request);  
 		log.debug("文件数量:" + fileList.size());
 		 //迭代器,搜索前端发送过来的文件
@@ -75,12 +127,14 @@ public class FileController {
 	 */
 	private List getFileList(HttpServletRequest request) {
 		DiskFileItemFactory fac = new DiskFileItemFactory();
+		log.debug("DiskFileItemFactory"+fac);
 		ServletFileUpload upload = new ServletFileUpload(fac);
+		log.debug("ServletFileUpload"+upload);
 		upload.setHeaderEncoding("utf-8");
 		List fileList = null;
 		try {
 			fileList= upload.parseRequest(request);
-			
+			log.debug("得到文件列表："+fileList.size());
 		} catch (FileUploadException e) {
 			throw new RuntimeException(e);
 		}
@@ -96,11 +150,14 @@ public class FileController {
 	private File getTempFile(HttpServletRequest request,String name,String type){
 		// 扩展名格式： extName就是文件的后缀,例如 .txt
         String extName = FilenameUtils.getExtension(name);
+        log.debug("文件扩展名："+extName);
         //备份文件的路径
         String savePath = getTempFilePath(request,type);
+        log.debug("临时文件目录："+savePath);
         String newName = UUID.randomUUID().toString();
         // 生成文件名：
         File saveFile = new File(savePath + newName + "."+extName);
+        log.debug("生成文件："+savePath.getBytes());
         return saveFile;
 	}
 	/**
@@ -123,14 +180,6 @@ public class FileController {
 	public String getFtpURL(String ip,String name,String password,String ftpPath){
 		return "ftp://"+name+":"+password+"@"+ip+ftpPath;
 	}
-	/**
-	 * 执行证照上传
-	 */
-	@SuppressWarnings({ "unchecked", "unused", "rawtypes"})
-	@RequestMapping(value="/uploadFile")
-	public void fileUpload(HttpServletRequest request,HttpServletResponse response){
-		this.uploadFTP(request, response, SystemConstant.FTP_PATH_LICENCE);
-	}
 	
 	/**
 	 * 上传ftp服务器
@@ -139,14 +188,16 @@ public class FileController {
 	public  String  uploadFtp(File file,String ftpPath){
 		//创建ftp  
         FTPClient ftpClient = new FTPClient();  
+        log.debug("创建 ftp："+ftpClient);
         ByteArrayInputStream bis = null;
-        byte[] buffer = null;
         // 建立FTP连接  
         try {
         		//链接ftp
 			ftpClient.connect(SystemConstant.FTP_IP);
+			log.debug("链接ftp："+ftpClient.getStatus());
 			//判断是否登录成功
 			if(ftpClient.login(SystemConstant.FTP_NAME, SystemConstant.FTP_PASSWORD)){
+				log.debug("ftp链接成功");
 				//判断路径
 				if(ftpClient.changeWorkingDirectory(ftpPath)){
 						if(null != file){
@@ -199,7 +250,8 @@ public class FileController {
         FTPClient ftpClient = new FTPClient();  
         ByteArrayInputStream bis = null;
         byte[] buffer = null;
-        BASE64Decoder decoder = new BASE64Decoder();
+        @SuppressWarnings("restriction")
+		BASE64Decoder decoder = new BASE64Decoder();
         // 建立FTP连接  
         try {
         		//链接ftp
@@ -210,6 +262,7 @@ public class FileController {
 				if(ftpClient.changeWorkingDirectory(SystemConstant.FTP_PATH_REALNAME)){
 						
 						if(base64str!=null || base64str!=""){
+							@SuppressWarnings("restriction")
 							byte[] b = decoder.decodeBuffer(base64str);
 							ByteArrayOutputStream bos = new ByteArrayOutputStream();
 							//Base64解码  
