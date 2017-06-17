94fa91cf374a4e398095e920ee48bca41436deb8
lilisheng <lilisheng@lilisheng-ThinkPad-X240>
2017-06-17 08:05:10 +0800

    增加电子表单的编辑和查看

--- src/main/java/com/xjl/pt/form/controller/FormController.java
+++ src/main/java/com/xjl/pt/form/controller/FormController.java
@@ -1,15 +1,10 @@
 package com.xjl.pt.form.controller;
 
-
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,29 +12,73 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import com.xjl.pt.form.controller.SessionTools;
+import com.xjl.pt.form.controller.XJLResponse;
+import com.xjl.pt.form.controller.BootstrapGridTable;
 import com.xjl.pt.form.domain.Form;
+import com.xjl.pt.core.domain.DictItem;
+import com.xjl.pt.core.domain.User;
+import com.xjl.pt.core.service.DictItemService;
+import com.xjl.pt.core.tools.DictItemTools;
 import com.xjl.pt.form.service.FormService;
-
 /**
- * 表单控制器，用于显示表单内容和接受表单参数
- * @author leasonlive
+ * 表单控制器类
+ * @author ControllerCoderTools lilisheng
  *
  */
 @Controller
 @RequestMapping("/form")
 public class FormController {
 	@Autowired
+	private SessionTools sessionTools;
+	@Autowired
+	private DictItemService dictItemService;
+	@Autowired
 	private FormService formService;
-	private static final Logger logger = LoggerFactory.getLogger(FormController.class);
+	@ResponseBody
+	@RequestMapping(value="/query/{page}/{rows}",method=RequestMethod.GET,consumes = "application/json")
+	public BootstrapGridTable query(HttpServletRequest request, @PathVariable Integer page,@PathVariable Integer rows){
+		String search = StringUtils.trimToNull(request.getParameter("search"));
+		List<Form> list = this.formService.query(search, page, rows);
+		//处理字典
+		//处理外键
+		return BootstrapGridTable.getInstance(list);
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/query/{id}",method=RequestMethod.GET,consumes = "application/json")
+	public Form queryById(HttpServletRequest request, @PathVariable String id){
+		Form form = this.formService.queryById(id);
+		//处理字典
+		//处理外键
+		return form;
+	}
+
 	@ResponseBody
 	@RequestMapping(value="/add",method=RequestMethod.POST,consumes = "application/json")
-	public void add(@RequestBody Form form){
-		this.formService.add(form);
+	public XJLResponse add(HttpServletRequest request, @RequestBody Form form){
+		User user = this.sessionTools.getUser(request);
+		this.formService.add(form, user);
+		return XJLResponse.successInstance();
 	}
+
 	@ResponseBody
-	@RequestMapping(value="/all/{page}/{rows}",method=RequestMethod.GET,consumes = "application/json")
-	public List<Form> all(HttpServletRequest request,@PathVariable Integer page,@PathVariable Integer rows){
-		return this.formService.query(page, rows);
+	@RequestMapping(value="/modify",method=RequestMethod.POST,consumes = "application/json")
+	public XJLResponse modify(HttpServletRequest request, @RequestBody Form form){
+		User user = this.sessionTools.getUser(request);
+		this.formService.modify(form, user);
+		return XJLResponse.successInstance();
 	}
+
+	@ResponseBody
+	@RequestMapping(value="/delete",method=RequestMethod.POST,consumes = "application/json")
+	public XJLResponse delete(HttpServletRequest request, @RequestBody List<Form> list){
+		User user = this.sessionTools.getUser(request);
+		for (Form form : list) {
+			this.formService.delete(form, user);
+		}
+		return XJLResponse.successInstance();
+	}
+
 }
+
