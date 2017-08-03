c34455285864dfaee430cad3fe08a9c73c38676c
alilusha <7141431@qq.com>
2017-08-03 15:22:52 +0800

    云终端访问日志

---
+++ src/main/java/com/xjl/cdc/cloud/controller/CdcTerminalLogController.java
@@ -0,0 +1,68 @@
+package com.xjl.cdc.cloud.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.xjl.cdc.cloud.domain.CdcTerminalLog;
+import com.xjl.cdc.cloud.service.CdcTerminalLogService;
+import com.xjl.pt.core.domain.User;
+import com.xjl.pt.form.controller.BootstrapGridTable;
+import com.xjl.pt.form.controller.SessionTools;
+import com.xjl.pt.form.controller.XJLResponse;
+
+/**
+ * 云检测中心终端日志记录控制器类
+ * @author you.guess
+ *
+ */
+@Controller
+@RequestMapping("/cdcTerminalLog")
+public class CdcTerminalLogController {
+	@Autowired
+	private SessionTools sessionTools;
+	@Autowired
+	private CdcTerminalLogService cdcTerminalLogService;
+	@ResponseBody
+	@RequestMapping(value="/query/{page}/{rows}",method=RequestMethod.GET,consumes = "application/json")
+	public BootstrapGridTable query(HttpServletRequest request, @PathVariable Integer page,@PathVariable Integer rows){
+		String search = StringUtils.trimToNull(request.getParameter("search"));
+		List<CdcTerminalLog> list = this.cdcTerminalLogService.query(search, page, rows);
+		
+		return BootstrapGridTable.getInstance(list);
+	}
+	@ResponseBody
+	@RequestMapping(value="/query/{id}",method=RequestMethod.GET,consumes = "application/json")
+	public CdcTerminalLog queryById(HttpServletRequest request, @PathVariable String id){
+		CdcTerminalLog cdcTerminalLog = this.cdcTerminalLogService.queryById(id);
+		return cdcTerminalLog;
+	}
+	@ResponseBody
+	@RequestMapping(value="/query/guid/{guid}",method=RequestMethod.GET,consumes = "application/json")
+	public BootstrapGridTable queryByGUID(HttpServletRequest request, @PathVariable String guid){
+		List<CdcTerminalLog> list = this.cdcTerminalLogService.queryByGUID(guid);
+		return BootstrapGridTable.getInstance(list);
+	}
+	@ResponseBody
+	@RequestMapping(value="/query/tmid/{tmid}",method=RequestMethod.GET,consumes = "application/json")
+	public BootstrapGridTable queryByTMID(HttpServletRequest request, @PathVariable String tmid){
+		List<CdcTerminalLog> list = this.cdcTerminalLogService.queryByTMId(tmid);
+		return BootstrapGridTable.getInstance(list);
+	}
+	@ResponseBody
+	@RequestMapping(value="/add",method=RequestMethod.POST,consumes = "application/json")
+	public XJLResponse add(HttpServletRequest request, @RequestBody CdcTerminalLog cdcTerminalLog){
+		User user = this.sessionTools.getUser(request);
+		this.cdcTerminalLogService.add(cdcTerminalLog, user);
+		return XJLResponse.successInstance();
+	}
+}
